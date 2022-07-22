3fc6c731237316b6c1fab4ee5e55438ee9803fa9
SivakKaterina <72225534+SivakKaterina@users.noreply.github.com>
2022-07-22 16:54:22 +0300

    add files

---
+++ js/scriptnew.js
@@ -0,0 +1,70 @@
+document.addEventListener("DOMContentLoaded", () => {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 5,
+        spaceBetween: 30,
+      },
+    },
+  });
+
+  AOS.init({
+    once: true,
+  });
+
+  var image = document.getElementsByClassName("bannerParalax");
+  new simpleParallax(image);
+
+  function mobileNav() {
+    if (document.querySelector(".smobitrigger")) {
+      const btn = document.querySelector(".smobitrigger");
+      const body = document.querySelector("body");
+      const menu = document.querySelector(".mobimenu");
+      const overlay = document.querySelector(".mobimenu-overlay");
+
+      btn.addEventListener("click", (event) => {
+        event.preventDefault();
+        if (btn.getAttribute("aria-expanded") === "false") {
+          btn.classList.add("is-active");
+          body.classList.add("nav-active");
+          btn.setAttribute("aria-expanded", "true");
+          menu.classList.add("is-active");
+          overlay.classList.add("is-active");
+        } else {
+          btn.classList.remove("is-active");
+          body.classList.remove("nav-active");
+          btn.setAttribute("aria-expanded", "false");
+          menu.classList.remove("is-active");
+          overlay.classList.remove("is-active");
+        }
+      });
+      overlay.addEventListener("click", (event) => {
+        event.preventDefault();
+        btn.classList.remove("is-active");
+        body.classList.remove("nav-active");
+        btn.setAttribute("aria-expanded", "false");
+        menu.classList.remove("is-active");
+        overlay.classList.remove("is-active");
+      });
+    }
+  }
+  mobileNav();
+});
